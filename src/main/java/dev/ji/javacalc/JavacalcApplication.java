04a9756c5595f75351a391187e4fb3e1eff6d9fb
yperunov <perunovyuri@gmail.com>
2022-01-29 09:43:29 +0300

    Initial commit

---
+++ src/main/java/dev/ji/javacalc/JavacalcApplication.java
@@ -0,0 +1,13 @@
+package dev.ji.javacalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavacalcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavacalcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/ji/javacalc/JavacalcApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ji.javacalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavacalcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
